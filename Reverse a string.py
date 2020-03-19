93616af04a4dbf4b83541c280588f6408f6052c6
Placid Serene <t.dung.lhp@gmail.com>
2020-03-19 14:53:16 -0700

    Bubble Sort Implementation

--- Reverse a string.py
+++ Reverse a string.py
@@ -1,12 +1,16 @@
-#Implement a function that reverses a string using iteration...and then recursion!
+# Implement a function that reverses a string using iteration...and then recursion!
 def reversedString(s):
     stringreversed = ""
     for i in range(len(s)-1, -1, -1):
         stringreversed = stringreversed + s[i]
     return stringreversed
 
-print(reversedString('yoyo mastery'))
+def recursionReversed(s):
+    if len(s) == 1:
+        return s
+    return s[-1] + recursionReversed(s[:len(s)-1])
 
-    
+print(recursionReversed('yoyo mastery'))    
+print(reversedString('yoyo mastery'))
 
 
---
+++ bubble sort.py
@@ -0,0 +1,18 @@
+# buble sort implementation
+
+a = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0]
+
+def bubblesort(arr):
+    i = len(arr) - 1
+    while i > 0:
+        print(i)
+        for j in range(i):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+         
+        i = i-1
+    return arr
+
+print(bubblesort(a))
+
+
